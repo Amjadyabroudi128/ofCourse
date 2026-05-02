fa69b48c382306541644f3c85b1fa16bcefcd091
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-02 18:16:54 +0100

    home icon

--- lib/core/appIcons.dart
+++ lib/core/appIcons.dart
@@ -4,4 +4,5 @@ import 'package:flutter/material.dart';
  class myIcons {
    static Icon profile = Icon(Icons.person);
    static Icon search = Icon(Icons.search);
+   static Icon home = Icon(Icons.home);
  }
