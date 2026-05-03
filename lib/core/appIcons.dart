2d4def1ce5266ea7fb3e401993c73d9c1b01f0c0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-03 13:52:18 +0100

    notifications icon

--- lib/core/appIcons.dart
+++ lib/core/appIcons.dart
@@ -5,4 +5,5 @@ import 'package:flutter/material.dart';
    static Icon profile = Icon(Icons.person);
    static Icon search = Icon(Icons.search);
    static Icon home = Icon(Icons.home);
+   static Icon notis = Icon(Icons.notifications);
  }
