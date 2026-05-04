dba6ed2dcf52e689aa944adfa2e6175d28e1dd09
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-04 18:37:32 +0100

    app bar profile

--- lib/ProfilePage/ProfilePage.dart
+++ lib/ProfilePage/ProfilePage.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:ofcourse/core/colors.dart';
+
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Profile Page",
-        style: TextStyle(fontSize: 30),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: myColors.appBar,
+        centerTitle: true,
+        title: Text("Profile"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(Icons.list, size: 30,),
+          ),
+        ],
       ),
     );
   }
