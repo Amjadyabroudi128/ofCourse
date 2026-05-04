afa1aa08357140ed90c25ffd23f44a52c2c728e0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-04 18:38:20 +0100

    list icon

--- lib/core/appIcons.dart
+++ lib/core/appIcons.dart
@@ -6,4 +6,5 @@ import 'package:flutter/material.dart';
    static Icon search = Icon(Icons.search);
    static Icon home = Icon(Icons.home);
    static Icon notis = Icon(Icons.notifications);
+   static Icon myList = Icon(Icons.list, size: 30,);
  }
