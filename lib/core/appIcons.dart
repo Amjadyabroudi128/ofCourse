987f24680decdb1ad39ce86f6d01747a7bd5ea2e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-06 20:50:11 +0100

    fix class name

--- lib/core/appIcons.dart
+++ lib/core/appIcons.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 
- class myIcons {
+ class MyIcons {
    static Icon profile = Icon(Icons.person);
    static Icon search = Icon(Icons.search);
    static Icon home = Icon(Icons.home);
