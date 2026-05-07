8ff39443be2a587d86dfd7491aeed5d74830539a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-07 16:06:53 +0100

    card component

---
+++ lib/components/card.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyCard extends StatelessWidget {
+  final Widget? child;
+  final ShapeBorder? shape;
+  final Clip? clipBehavior;
+  final Color? color;
+  final double? elevation;
+  const MyCard({super.key, this.child, this.shape, this.clipBehavior, this.color, this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      color: color,
+      shape: shape,
+      clipBehavior: clipBehavior,
+      child: child,
+    );
+  }
+}
