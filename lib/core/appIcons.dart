a58a67248a351d9f54c502c62bfee0b5dd4b5236
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-09 19:01:04 +0100

    ualifications icon

--- lib/core/appIcons.dart
+++ lib/core/appIcons.dart
@@ -11,4 +11,6 @@ import 'colors.dart';
    static Icon myList = Icon(Icons.list, size: 30,);
    static Icon forward = Icon(Icons.arrow_forward_ios);
    static Icon file = Icon(Icons.upload_file_outlined, color: MyColors.appBar);
+   static Icon qualifications = Icon(Icons.school_outlined, color: MyColors.appBar);
+
  }
