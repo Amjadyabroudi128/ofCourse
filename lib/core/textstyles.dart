1dbe35ef747d0af79abd3ac64d99dcad92a14a73
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-11 21:36:40 +0100

    job matches styles

--- lib/core/textstyles.dart
+++ lib/core/textstyles.dart
@@ -4,4 +4,5 @@ import 'package:flutter/material.dart';
 import 'colors.dart';
 class MyStyles {
   static TextStyle ofCourse = TextStyle(color: MyColors.ofCourse);
+  static TextStyle matches = TextStyle(fontWeight: FontWeight.w600, color: Colors.grey[600]);
 }
