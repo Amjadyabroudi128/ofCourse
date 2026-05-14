8c5e15fb21cab701890fa9a054b67bb392f90108
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-14 14:35:36 +0100

    text style color

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
  class MyColors {
    static Color ofCourse = Colors.blueAccent;
    static Color? appBar = Colors.grey[350];
+   static Color? cv = Colors.grey[600];
  }
