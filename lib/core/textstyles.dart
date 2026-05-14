e5dc73294a08fb0af59757dd6871398e8546e1b7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-14 14:35:27 +0100

    text style

--- lib/core/textstyles.dart
+++ lib/core/textstyles.dart
@@ -4,5 +4,5 @@ import 'package:flutter/material.dart';
 import 'colors.dart';
 class MyStyles {
   static TextStyle ofCourse = TextStyle(color: MyColors.ofCourse);
-  static TextStyle matches = TextStyle(fontWeight: FontWeight.w600, color: Colors.grey[600]);
+  static TextStyle matches = TextStyle(fontWeight: FontWeight.w600, color: MyColors.cv);
 }
