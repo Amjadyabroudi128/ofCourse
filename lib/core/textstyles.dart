e5665f826f652d346ca9dab66fb07fa8287362db
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-05-17 22:09:16 +0100

    font bold

--- lib/core/textstyles.dart
+++ lib/core/textstyles.dart
@@ -1,8 +1,9 @@
 
 import 'package:flutter/material.dart';
+import 'package:ofcourse/core/fontWeight.dart';
 
 import 'colors.dart';
 class MyStyles {
   static TextStyle ofCourse = TextStyle(color: MyColors.ofCourse);
-  static TextStyle matches = TextStyle(fontWeight: FontWeight.w600, color: MyColors.cv);
+  static TextStyle matches = TextStyle(fontWeight: MyFonts.fontBold, color: MyColors.cv);
 }
